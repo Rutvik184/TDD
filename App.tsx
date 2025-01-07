2794badc27f4b4e41e5369011f29c8f5d053e9b1
RutvikPatel <rutvik2000.rp@gmail.com>
2025-01-07 22:46:43 +0530

    [New] Design text input and button, [Update] README.md file for the instructions.

--- App.tsx
+++ App.tsx
@@ -5,113 +5,70 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  Alert,
+  FlatList,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App(): React.JSX.Element {
+  const [inputValue, setInputValue] = useState('');
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <SafeAreaView />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Here..."
+        value={inputValue}
+        onChangeText={setInputValue}
+      />
+      <TouchableOpacity
+        style={styles.btnStyle}
+        onPress={() => {}}
+        testID="handleSubmit">
+        <Text style={styles.btnName}>Submit</Text>
+      </TouchableOpacity>
+
+      <SafeAreaView />
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
+  btnName: {
+    color: 'white',
     fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
     fontWeight: '700',
+  },
+  btnStyle: {
+    width: '50%',
+    backgroundColor: 'black',
+    paddingVertical: 10,
+    borderRadius: 10,
+    alignSelf: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  input: {
+    borderColor: '#00000030',
+    borderWidth: 1,
+    color: 'black',
+    borderRadius: 5,
+    fontWeight: '600',
+    paddingHorizontal: 10,
+    height: 40,
+  },
+  container: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+    padding: 20,
   },
 });
 
