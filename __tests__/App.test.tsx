e5239271d99b87f83bdde9185b862aef7691e2e4
RutvikPatel <rutvik2000.rp@gmail.com>
2025-01-07 22:55:47 +0530

    [Nw] write test cases for render component currectlty and update input value currectly.

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,18 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
+import {render, fireEvent} from '@testing-library/react-native';
 import App from '../App';
 
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
+describe('App Component with Input Handling', () => {
+  it('renders correctly', async () => {
+    const {getByPlaceholderText, getByText} = render(<App />);
+    expect(getByPlaceholderText('Enter Here...')).toBeTruthy();
+    expect(getByText('Submit')).toBeTruthy();
+  });
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+  it('updates input value correctly', () => {
+    const {getByPlaceholderText} = render(<App />);
+    const input = getByPlaceholderText('Enter Here...');
+    fireEvent.changeText(input, '123');
+    expect(input.props.value).toBe('123');
+  });
 });
